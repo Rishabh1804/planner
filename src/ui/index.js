125442c4b90d1508825912605055d7180a8c81d8
Rishabh Jain <Rishabh1804@gmail.com>
2026-05-05 18:06:33 +0530

    ui: export options view modules

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,6 +1,6 @@
 // Holiday Planner UI module boundary.
 //
-// Non-wired. Future UI extraction should import DOM, tab, and Options cockpit helpers here.
+// Non-wired. Future UI extraction should import DOM, tab, and Options helpers here.
 
 export {
   createElement,
@@ -28,3 +28,23 @@ export {
   isOptionsViewModel,
   normalizeOptionRecommendation
 } from './options-cockpit.js';
+
+export {
+  OPTIONS_VIEW_TARGETS,
+  createOptionsView,
+  renderCompactRecommendationCard,
+  renderLensChip,
+  renderLensStrip,
+  renderOptionsCockpit,
+  renderOptionsPrimaryCard,
+  renderOptionsSecondaryPanels,
+  renderWinnerCard,
+  renderWinnerStrip
+} from './options-view.js';
+
+export {
+  OPTIONS_ACTIONS,
+  bindOptionsView,
+  setActiveOptionsLens,
+  showOptionsComparePanel
+} from './options-bindings.js';
